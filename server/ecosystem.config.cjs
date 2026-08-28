13c53c2e2e10af1e1f4de0f40d7953f8557ea41a
Prahlad Singh <prahladsingh7512@gmail.com>
2026-08-28 17:09:21 +0530

    Update ecosystem.config.cjs

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -3,13 +3,14 @@ module.exports = {
     {
       name: 'interviewvault-api',
       script: 'src/index.js',
-      cwd: '/home/prahlad/interviewvault/server',
+      cwd: '/var/www/projects/interviewvault/server',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
       env_production: {
         NODE_ENV: 'production',
+        PORT: 5004,
       },
     },
   ],
